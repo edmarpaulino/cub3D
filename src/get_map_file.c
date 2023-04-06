e34b9fac961ca128f941a0bd828f6b2f714ae507
Edmar Paulino <edmarpaulino9@gmail.com>
2023-04-05 21:04:36 -0300

    refactor: remove prints

--- src/get_map_file.c
+++ src/get_map_file.c
@@ -6,53 +6,13 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 20:14:12 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/03/25 18:50:06 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/04/01 16:38:10 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3D.h"
 
 static void		init_map_file(t_map_file *map_file);
-
-static void	print_file_content(t_queue *file_content)
-{
-	t_queue_item	*content;
-
-	content = file_content->first;
-	while (content)
-	{
-		ft_putendl_fd(content->value, STDOUT);
-		content = content->next;
-	}
-}
-
-static void	print_matrix(char **matrix)
-{
-	int	row;
-
-	row = 0;
-	while (matrix[row])
-	{
-		ft_putendl_fd(matrix[row], STDOUT);
-		row++;
-	}
-}
-
-static void	print_map_file(t_map_file *map_file)
-{
-	printf("\n\n");
-	print_matrix(map_file->matrix);
-	printf("texture_no = %s\n", map_file->texture_no);
-	printf("texture_so = %s\n", map_file->texture_so);
-	printf("texture_ea = %s\n", map_file->texture_ea);
-	printf("texture_we = %s\n", map_file->texture_we);
-	printf("ceil_color = %d\n", map_file->ceil_color);
-	printf("floor_color = %d\n", map_file->floor_color);
-	printf("player_direction = %c\n", map_file->player_direction);
-	printf("player_y = %d\n", map_file->player_y);
-	printf("player_x = %d\n", map_file->player_x);
-	printf("\n\n");
-}
 
 t_map_file	*get_map_file(char *filename)
 {
@@ -65,11 +25,12 @@ t_map_file	*get_map_file(char *filename)
 	init_map_file(map_file);
 	map_file->file_content = get_file_content(filename);
 	if (!map_file->file_content)
+	{
+		free_map_file(map_file);
 		exit_error("Cannot get file content");
+	}
 	load_textures(map_file);
 	load_map(map_file);
-	print_map_file(map_file);
-	print_file_content(map_file->file_content);
 	return (map_file);
 }
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 19:11:36 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/03/25 20:52:37 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/03/31 20:23:27 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	main(int argc, char *argv[])
 	if (argc > 2)
 		exit_error("The program requires only one map file");
 	map_file = get_map_file(argv[1]);
-	ft_putendl_fd("Welcome to the cub3D", STDOUT);
 	setup(&data, map_file);
 	key_hook(data);
 	game(data);
